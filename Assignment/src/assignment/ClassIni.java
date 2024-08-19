a28bd7ab91744e353aa3d7ec2c6e8fc6a55ebfaa
y <yyapwt@gmail.com>
2024-08-19 20:58:19 +0800

    trying to do ini

--- Assignment/src/assignment/ClassIni.java
+++ Assignment/src/assignment/ClassIni.java
@@ -1,7 +1,24 @@
 package assignment;
 
+import java.io.*;
+
 public class ClassIni {
-    try {
-        
-    } catch
+    public void Ini() {
+        try {
+            BufferedReader rd = new BufferedReader(new FileReader("users.txt"));
+        } catch (FileNotFoundException e) {
+            try {
+                BufferedWriter w = new BufferedWriter(new FileWriter("users.txt"));
+                UserMangement id = new UserMangement();
+                w.write("");
+            } catch (IOException e){
+                System.out.println("Error when adding a new user. ");               
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error Found in reader file");
+        }
+    }
+    
+
 }
