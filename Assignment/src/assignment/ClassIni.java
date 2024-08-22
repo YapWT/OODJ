7eeac580bf618f48a9673a429aaed2e7ce20418e
y <yyapwt@gmail.com>
2024-08-22 20:00:47 +0800

    Update ClassIni.java

--- Assignment/src/assignment/ClassIni.java
+++ Assignment/src/assignment/ClassIni.java
@@ -7,20 +7,9 @@ public class ClassIni {
         try (BufferedReader rd = new BufferedReader(new FileReader("users.txt"))) {
             return null;
         } catch (FileNotFoundException e) {
-            try (BufferedWriter w = new BufferedWriter(new FileWriter("users.txt"))) {
-                
-                UserMangement id = new UserMangement();
-                String ID = id.generateID("A");
-                w.write(String.format("%s,123,admin,A,active", ID));
-                
-                return (String.format("ID: %s\nPasswors: 123", ID));
-                
-            } catch (IOException e2){
-                System.out.println("Error when adding a new user. ");
-                return null;
-            }
-        }
-        catch (IOException e) {
+            new zWriteFile().write("users.txt", "A160001,123,admin,A,pending", false);
+            return "A160001,123,admin,A,pending";
+        } catch (IOException e) {
             System.out.println("Error Found in reader file");
             return null;
         }
