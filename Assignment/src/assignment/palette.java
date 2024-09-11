f9c3dd05d8c776d8a6fa0b80f913414387b9621e
y <yyapwt@gmail.com>
2024-09-11 22:52:59 +0800

    dont know how to explain

--- Assignment/src/assignment/palette.java
+++ Assignment/src/assignment/palette.java
@@ -2,13 +2,17 @@ package assignment;
 
 import javax.swing.*;
 
-public class yGetRadioButtonValue {
-    public String getSelectedButton(ButtonGroup group) {
+public abstract class palette {
+    abstract String getValue(ButtonGroup group);
+}
+
+class getSelectedButton extends palette {
+    @Override
+    public String getValue(ButtonGroup group) {
         for (var buttons = group.getElements(); buttons.hasMoreElements();) {
             AbstractButton button = buttons.nextElement();
-            if (button.isSelected()) {
+            if (button.isSelected())
                 return button.getText();
-            }
         }
         return null;
     }
