444a6332bca87169c3aa81663c1876bf3f750dec
y <yyapwt@gmail.com>
2024-09-16 15:39:34 +0800

    Update Admin.java
    bug

--- Assignment/src/assignment/Admin.java
+++ Assignment/src/assignment/Admin.java
@@ -2,6 +2,6 @@ package assignment;
 
 class Admin extends User{
     public void addStaff(String Uname, String Upass, String Utype, String Ucontact) {
-        super.addUser(Uname, Upass, Utype, Ucontact);
+        super.addUser(Uname, Upass, Utype.substring(0, 1), Ucontact);
     }
 }
