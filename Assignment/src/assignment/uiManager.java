89b0008f332de1d8d66b018e37755a192b646e4a
TP075205 <yewtiew59@gmail.com>
2024-09-18 02:42:24 +0800

    manager ui

--- Assignment/src/assignment/uiManager.java
+++ Assignment/src/assignment/uiManager.java
@@ -16,21 +16,105 @@ public class uiManager extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        salesDashboardBtn = new javax.swing.JButton();
+        maintenanceOperationBtn = new javax.swing.JButton();
+        evaluatePerformanceBtn = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel3.setBackground(new java.awt.Color(204, 204, 204));
+
+        salesDashboardBtn.setBackground(new java.awt.Color(102, 102, 102));
+        salesDashboardBtn.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        salesDashboardBtn.setForeground(new java.awt.Color(255, 255, 255));
+        salesDashboardBtn.setText("Sales Dashboard");
+        salesDashboardBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                salesDashboardBtnActionPerformed(evt);
+            }
+        });
+
+        maintenanceOperationBtn.setBackground(new java.awt.Color(102, 102, 102));
+        maintenanceOperationBtn.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        maintenanceOperationBtn.setForeground(new java.awt.Color(255, 255, 255));
+        maintenanceOperationBtn.setText("Maintenance Operation");
+
+        evaluatePerformanceBtn.setBackground(new java.awt.Color(102, 102, 102));
+        evaluatePerformanceBtn.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        evaluatePerformanceBtn.setForeground(new java.awt.Color(255, 255, 255));
+        evaluatePerformanceBtn.setText("Evaluate Staff Performance");
+
+        jButton1.setBackground(new java.awt.Color(102, 102, 102));
+        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Edit Account");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(44, 44, 44)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1)
+                    .addComponent(evaluatePerformanceBtn)
+                    .addComponent(maintenanceOperationBtn)
+                    .addComponent(salesDashboardBtn))
+                .addContainerGap(74, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(82, 82, 82)
+                .addComponent(salesDashboardBtn)
+                .addGap(38, 38, 38)
+                .addComponent(maintenanceOperationBtn)
+                .addGap(44, 44, 44)
+                .addComponent(evaluatePerformanceBtn)
+                .addGap(43, 43, 43)
+                .addComponent(jButton1)
+                .addContainerGap(91, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(386, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void salesDashboardBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_salesDashboardBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_salesDashboardBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -68,5 +152,11 @@ public class uiManager extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton evaluatePerformanceBtn;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JButton maintenanceOperationBtn;
+    private javax.swing.JButton salesDashboardBtn;
     // End of variables declaration//GEN-END:variables
 }
