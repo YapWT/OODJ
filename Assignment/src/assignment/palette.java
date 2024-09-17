7cdba882d868b3a0374bd742b77ec67798dc4bb6
y <yyapwt@gmail.com>
2024-09-17 17:49:41 +0800

    Update palette.java
    delete unuse method

--- Assignment/src/assignment/palette.java
+++ Assignment/src/assignment/palette.java
@@ -6,8 +6,6 @@ import javax.swing.table.DefaultTableModel;
 
 public class palette {
     public String getValue(ButtonGroup group) { return null; }
-    public void set(boolean b){}
-    public void deleteAll() {}
     public boolean emptyPassword() {return true; }
 }
 
@@ -29,25 +27,6 @@ class ComponentAction extends palette {
 //    new ComponentAction(new ArrayList<>(Arrays.asList(TXT_name))).deleteAll();
     public ComponentAction (ArrayList<JComponent> object) {
         this.object = object;
-    }
-    
-    @Override
-    public void set(boolean b) {
-        for (JComponent ob : object) {
-            ob.setEnabled(b);
-        }
-    }
-    
-    @Override
-    public void deleteAll() {
-        for (JComponent ob : object) {
-            if (ob instanceof JTextField)
-                ((JTextField) ob).setText("");
-            else if (ob instanceof JPasswordField)
-                ((JPasswordField) ob).setText("");
-            else
-                System.out.println("Error on deleteAll()");
-        }
     }
     
     @Override
