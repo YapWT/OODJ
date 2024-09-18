6c9ad5482a8075c7db846865f6f923b4738e8a56
Aaron Ho <hoyanxun@gmail.com>
2024-09-18 10:41:51 +0800

    Hall

---
+++ Assignment/src/assignment/Hall.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment;
+
+/**
+ * @author aaron
+ */
+public class Hall {
+  private int hallID;
+  private String hallType;
+  private int capacity;
+  private int ratePerHour;
+  private String[] timeSlots = new String[12];
+}
