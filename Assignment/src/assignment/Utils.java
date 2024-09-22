8d87391909443fd94352921f84edb2e4bd169880
Aaron Ho <hoyanxun@gmail.com>
2024-09-22 16:50:57 +0800

    Changes to hall

--- Assignment/src/assignment/Utils.java
+++ Assignment/src/assignment/Utils.java
@@ -34,7 +34,7 @@ public class Utils {
           data = FileOperations.read("halls.txt");
           for (Object obj : data) {
             Hall hall = (Hall) obj;
-            typeOfData.add(Integer.toString(hall.getHallID()));
+            typeOfData.add(hall.getHallID());
           }
           break;
         }
@@ -53,55 +53,63 @@ public class Utils {
   public static <T> T IDtoObject(String id, String filename, Class<T> runtimeClass) {
     ArrayList<T> objects = FileOperations.read(filename);
     ArrayList<Character> userTypes = new ArrayList<>(Arrays.asList('A', 'S', 'C', 'M'));
-    
+
     char IDtype = id.charAt(0);
-    
+
     if (runtimeClass == User.class && userTypes.contains(IDtype)) {
-        for (T obj : objects) {
-            User user = (User) obj;
-            if (user.getUid().equals(id))
-                return obj;
-        }
+      for (T obj : objects) {
+        User user = (User) obj;
+        if (user.getUid().equals(id)) return obj;
+      }
     } else if (runtimeClass == Payment.class && IDtype == 'P') {
-        for (T obj : objects) {
-            Payment payment = (Payment) obj;
-            if (payment.getPaymentID().equals(id)) {
-                return obj;
-            }
+      for (T obj : objects) {
+        Payment payment = (Payment) obj;
+        if (payment.getPaymentID().equals(id)) {
+          return obj;
         }
-    } 
+      }
+    }
     return null;
   }
 
   // edit one column of a row
-  public static <T> void editFile(String filename, String id, int column, String newData, Class<T> runtimeClass) {
+  public static <T> void editFile(
+      String filename, String id, int column, String newData, Class<T> runtimeClass) {
     ArrayList<T> obj = FileOperations.read(filename);
 
     if (runtimeClass == User.class) {
-        for (T row : obj) {
-            User user = (User) row;
-            if (user.getUid().equals(id)) {
-                setColumnValue(column, newData, row);
-                break;
-            }
+      for (T row : obj) {
+        User user = (User) row;
+        if (user.getUid().equals(id)) {
+          setColumnValue(column, newData, row);
+          break;
         }
+      }
     }
     // edit with rewrite all data
     FileOperations.write(filename, obj);
   }
-  
+
   private static <T> void setColumnValue(int column, String value, T row) {
-      // id is not avalable to make modify
-      if (row instanceof User) {
-          // type is not avaible to edit
-          User user = (User) row; 
-          switch (column) {
-              case 1: user.setUpass(value);break;
-              case 2: user.setUname(value);break;
-              case 4: user.setUcontact(value);break;
-              case 5: user.setUstatus(value);break;
-          }
+    // id is not avalable to make modify
+    if (row instanceof User) {
+      // type is not avaible to edit
+      User user = (User) row;
+      switch (column) {
+        case 1:
+          user.setUpass(value);
+          break;
+        case 2:
+          user.setUname(value);
+          break;
+        case 4:
+          user.setUcontact(value);
+          break;
+        case 5:
+          user.setUstatus(value);
+          break;
       }
+    }
   }
 
   public static boolean checkContact(String contact) {
@@ -109,3 +117,4 @@ public class Utils {
     return false;
   }
 }
+
