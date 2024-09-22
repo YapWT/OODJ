8c690962cda0f7b58ab439670dd524c789b57be3
Zhu Sheng <yapzhusheng@gmail.com>
2024-09-22 16:41:47 +0800

    remove timeslot from hall

--- Assignment/src/assignment/Hall.java
+++ Assignment/src/assignment/Hall.java
@@ -5,16 +5,14 @@ public class Hall {
   private String hallType;
   private int capacity;
   private int ratePerHour;
-  private String[] timeSlots = new String[10];
 
   public Hall() {}
 
-  public Hall(String id, String type, int capacity, int ratePerHour, String[] timeSlots) {
+  public Hall(String id, String type, int capacity, int ratePerHour) {
     this.hallID = id;
     this.hallType = type;
     this.capacity = capacity;
     this.ratePerHour = ratePerHour;
-    this.timeSlots = timeSlots;
   }
 
   public String getHallID() {
@@ -33,18 +31,6 @@ public class Hall {
     return ratePerHour;
   }
 
-  public String[] getTimeSlots() {
-    return timeSlots;
-  }
-
-  public String getTimeSlot(int index) {
-    if (index >= 0 && index < timeSlots.length) {
-      return timeSlots[index];
-    } else {
-      throw new IllegalArgumentException("Index out of bounds");
-    }
-  }
-
   public void setHallID(String hallID) {
     this.hallID = hallID;
   }
@@ -60,43 +46,7 @@ public class Hall {
   public void setRatePerHour(int ratePerHour) {
     this.ratePerHour = ratePerHour;
   }
-
-  public void setTimeSlots(String[] timeSlots) {
-    if (timeSlots != null && timeSlots.length == 12) {
-      this.timeSlots = timeSlots;
-    } else {
-      throw new IllegalArgumentException("Time slots must be an array of 10 elements.");
-    }
-  }
-
-  public void setTimeSlot(int index, String timeSlot) {
-    if (index >= 0 && index < timeSlots.length) {
-      this.timeSlots[index] = timeSlot;
-    } else {
-      throw new IllegalArgumentException("Index out of bounds");
-    }
-  }
-
-  public String toString() {
-    return String.format(
-        "%d,%s,%d,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s",
-        this.hallID,
-        this.hallType,
-        this.capacity,
-        this.ratePerHour,
-        this.timeSlots[0],
-        this.timeSlots[1],
-        this.timeSlots[2],
-        this.timeSlots[3],
-        this.timeSlots[4],
-        this.timeSlots[5],
-        this.timeSlots[6],
-        this.timeSlots[7],
-        this.timeSlots[8],
-        this.timeSlots[9]);
-  }
 }
-
 class Auditorium extends Hall {
   public Auditorium() {
         super();
@@ -105,8 +55,6 @@ class Auditorium extends Hall {
         setRatePerHour(300);
     }
 }
-
-
 
 class Banquet extends Hall {
 
@@ -119,7 +67,6 @@ class Banquet extends Hall {
 
 }
 
-
 class MeetingRoom extends Hall {
     public MeetingRoom() {
         super();
@@ -127,6 +74,6 @@ class MeetingRoom extends Hall {
         setCapacity(30);
         setRatePerHour(50);
     }
+    }
 
-}
 
