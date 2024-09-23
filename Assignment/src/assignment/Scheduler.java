5d9547c77680d62ceca905dca042e6ebe8716c3d
Aaron Ho <hoyanxun@gmail.com>
2024-09-23 13:54:50 +0800

    Fixed schedule.java

--- Assignment/src/assignment/Scheduler.java
+++ Assignment/src/assignment/Scheduler.java
@@ -1,47 +1,50 @@
 package assignment;
 
-import javax.swing.JTable;
-import javax.swing.table.TableModel;
-import java.util.ArrayList;
-import java.util.Vector;
-import javax.swing.table.DefaultTableModel;
-
 public class Scheduler extends Staff {
   public Scheduler() {}
+
   private String data;
   private String hallID;
 
   public String addHall(int hallType) {
-      data=null;
-      hallID = Utils.generateID("H");
-      switch(hallType){
-          case 0:
-          {
-            
-            Auditorium audi = new Auditorium();
-            audi.setHallID(hallID);
-            audi.setHallStatus("Active");
-            data = audi.getHallID()+", "+audi.getHallType()+", "+audi.getHallStatus();;
-          }
-          case 1:
-          {
-            Banquet banquet = new Banquet();
-            banquet.setHallID(hallID);
-            banquet.setHallStatus("Active");
-            data = banquet.getHallID()+", "+banquet.getHallType()+", "+banquet.getHallStatus();
-          }
-          case 2:
-          {
-            MeetingRoom meetingRoom = new MeetingRoom();
-            meetingRoom.setHallID(hallID);
-            meetingRoom.setHallStatus("Active");
-            data = meetingRoom.getHallID()+", "+meetingRoom.getHallType()+", "+meetingRoom.getHallStatus();
-          }
+    data = null;
+    hallID = Utils.generateID("H");
+    System.out.println(hallType);
+    switch (hallType) {
+      case 0:
+        {
+          Auditorium audi = new Auditorium();
+          audi.setHallID(hallID);
+          audi.setHallStatus("Active");
+          data = audi.getHallID() + "," + audi.getHallType() + "," + audi.getHallStatus();
+          break;
         }
-        FileOperations.write("halls.txt", data); 
-        return ("Hall Added");
+      case 1:
+        {
+          Banquet banquet = new Banquet();
+          banquet.setHallID(hallID);
+          banquet.setHallStatus("Active");
+          data = banquet.getHallID() + "," + banquet.getHallType() + "," + banquet.getHallStatus();
+          break;
+        }
+      case 2:
+        {
+          MeetingRoom meetingRoom = new MeetingRoom();
+          meetingRoom.setHallID(hallID);
+          meetingRoom.setHallStatus("Active");
+          data =
+              meetingRoom.getHallID()
+                  + ","
+                  + meetingRoom.getHallType()
+                  + ","
+                  + meetingRoom.getHallStatus();
+        }
+        break;
+    }
+    FileOperations.write("halls.txt", data);
+    return ("Hall Added");
   }
-    
+
   public void editHall() {}
 
   public void deleteHall() {}
