7a3fc6b29c556b123c67b6b0198d530d408c0eb6
Aaron Ho <hoyanxun@gmail.com>
2024-09-23 20:16:33 +0800

    Removed scheduled ID

--- Assignment/src/assignment/Schedule.java
+++ Assignment/src/assignment/Schedule.java
@@ -3,22 +3,16 @@ package assignment;
 import java.time.LocalDate;
 
 public class Schedule {
-  private String scheduleID;
   private LocalDate scheduleDate;
   private String hallID;
   private int[] timeSlot;
 
   public Schedule() {}
 
-  public Schedule(String scheduleID, LocalDate date, String hallID, int[] timeSlot) {
-    this.scheduleID = scheduleID;
+  public Schedule(LocalDate date, String hallID, int[] timeSlot) {
     this.scheduleDate = date;
     this.hallID = hallID;
     this.timeSlot = timeSlot;
-  }
-
-  public String getScheduleID() {
-    return scheduleID;
   }
 
   public LocalDate getScheduleDate() {
@@ -31,10 +25,6 @@ public class Schedule {
 
   public int[] getTimeSlot() {
     return timeSlot;
-  }
-
-  public void setScheduleID(String scheduleID) {
-    this.scheduleID = scheduleID;
   }
 
   public void setScheduleDate(LocalDate scheduleDate) {
@@ -52,11 +42,7 @@ public class Schedule {
   @Override
   public String toString() {
     return String.format(
-        "%s,%s,%s,%d,%d",
-        this.scheduleID,
-        this.scheduleDate.toString(),
-        this.hallID,
-        this.timeSlot[0],
-        this.timeSlot[1]);
+        "%s,%s,%d,%d",
+        this.scheduleDate.toString(), this.hallID, this.timeSlot[0], this.timeSlot[1]);
   }
 }
