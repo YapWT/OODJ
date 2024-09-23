c7e6a58869ac12a16506ee59f8394e54969fb4b6
y <yyapwt@gmail.com>
2024-09-23 18:18:12 +0800

    finish modify user's password method

--- Assignment/src/assignment/Admin.java
+++ Assignment/src/assignment/Admin.java
@@ -1,5 +1,8 @@
 package assignment;
 
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 class Admin extends Staff{
     public Admin() {}
     
@@ -12,5 +15,33 @@ class Admin extends Staff{
         this.setUid(Utils.generateID(getUtype()));
 
         FileOperations.write("users.txt", this);
-  }
+    }
+    
+    public boolean modifyPass(JTable t, String row, String pass) {
+        if (checkInt(row)) {
+            int n = Integer.parseInt(row);
+            User m = Utils.IDtoObject(rowToGetUid(t, n), "users.txt", User.class);
+            
+            if (m == null) return false;
+            
+            m.setUpass(pass);
+            Utils.editFile("users.txt", m.getUid(), 1, pass, User.class);
+            t.setValueAt(pass.replaceAll(".", "*"), n - 1, 2);
+            return true;
+        } return false;
+    }
+    
+    private boolean checkInt(String i) {
+        try {
+            Integer.parseInt(i);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    
+    private String rowToGetUid(JTable t, int row){
+        DefaultTableModel model = (DefaultTableModel) t.getModel();
+        return model.getValueAt(row - 1, 1).toString();
+    }
 }
